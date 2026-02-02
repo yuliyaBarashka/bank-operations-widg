c23a1ffb8b450c7c7eaf675f08c96a57708fa5d7
yuliyaBarashka <zohn12333@gmail.com>
2026-02-03 00:06:56 +0700

    init added module

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,4 @@ from tests import test_mask  # noqa: F401
 from tests import test_widget  # noqa: F401
 from tests import test_processing  # noqa: F401
 from tests import test_generators  # noqa: F401
+from tests import test_decorators   # noqa: F401
