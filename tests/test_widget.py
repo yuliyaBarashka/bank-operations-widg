5fccfed3c013b2178f7ab04b9cc48d8308400b14
yuliyaBarashka <zohn12333@gmail.com>
2026-02-02 17:37:20 +0700

    Add parametrize

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,19 +1,24 @@
+import pytest
 from src.widget import mask_account_card, get_date
 
 
-def test_mask_account_card_with_card():
-    data = "Visa Classic 1234567812345678"
-    expected = "Visa Classic 1234 56** **** 5678"
+@pytest.mark.parametrize(
+    "data, expected",
+    [
+        ("Visa Classic 1234567812345678", "Visa Classic 1234 56 **** 5678"),
+        ("Счет 40817810099910004312", "Счет **4312"),
+    ]
+)
+def test_mask_account_card(data, expected):
     assert mask_account_card(data) == expected
 
 
-def test_mask_account_card_with_account():
-    data = "Счет 40817810099910004312"
-    expected = "Счет **4312"
-    assert mask_account_card(data) == expected
-
-
-def test_get_date():
-    date_str = "2024-03-11T15:30:00"
-    expected = "11.03.2024"
+@pytest.mark.parametrize(
+    "date_str, expected",
+    [
+        ("2024-03-11T15:30:00", "11.03.2024"),
+        ("2023-01-01T00:00:00", "01.01.2023"),
+    ]
+)
+def test_get_date(date_str, expected):
     assert get_date(date_str) == expected
