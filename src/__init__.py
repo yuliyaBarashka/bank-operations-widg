0e4aabfce91ad8cc5d056e46654eb1b685829c22
yuliyaBarashka <zohn12333@gmail.com>
2026-02-02 23:53:00 +0700

    added new module decorators

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from src import mask  # noqa: F401
 from src import widget  # noqa: F401
 from src import processing  # noqa: F401
 from src import generators  # noqa: F401
+from src import decorators  # noqa: F401
